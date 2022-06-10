446dca20e2c04529d74f964427127b4007a55201
Wykeless <53314312+Wykeless@users.noreply.github.com>
2022-06-10 16:54:45 +0200

    Updated scaffold body

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,15 +12,72 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Row(),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment(0.2, -1.01), // near the top right
+          radius: 0.5,
+          colors: <Color>[
+            Color(0xFF1f4138), // green
+            Color(0xFF1e1d1e), // black
+          ],
+          stops: <double>[0.15, 1.0],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              ImageFiltered(
+                imageFilter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: RadialGradient(
+                      center: Alignment(-1.0, -0.5), // near the top right
+                      radius: 0.4,
+                      colors: <Color>[
+                        Color(0xFF1f4138), // green
+                        Colors.transparent, // black
+                      ],
+                      stops: <double>[0.25, 1.0],
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Image.asset(
+                      'assets/whatsapp_logo.png',
+                      width: 30,
+                      height: 30,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text(
+                      "Whatsapp",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
